53065d2fb38ad9dbf95d4fe14094a9d30b584a9c
omar <omar-malik22@hotmail.com>
2023-02-07 15:12:17 +0000

    created first route

---
+++ app.rb
@@ -0,0 +1,15 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Application < Sinatra::Base
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/hello' do
+    name = params[:name]
+
+    return "Hello #{name}"
+  end
+end
